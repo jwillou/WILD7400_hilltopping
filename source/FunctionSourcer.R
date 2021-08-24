428c2be5021d2fde2bac36975076a6d9b416a54b
Janna Willoughby <jwilloughby@auburn.edu>
2021-08-24 11:17:23 -0500

    outline model

---
+++ source/FunctionSourcer.R
@@ -0,0 +1,10 @@
+#FunctionSourcer.R
+
+#Set working directory, import packages, source functions, 
+setwd(paste(directory,"/source/", sep = ''))    # set temp working directory 
+
+#import packages
+#library()
+
+#source functions
+#source(paste(getwd(), "/RunModel.R", sep = ''))
---
+++ source/RunModel.R
@@ -0,0 +1,22 @@
+setwd("/Users/jannawilloughby/GDrive/ABMs/WILD7400_hilltopping/")
+directory = getwd()
+outdir    = paste(directory,"/output/", sep="")
+source(paste(directory, "/source/FunctionSourcer.R", sep =''))
+
+#parameters
+elevation = c(0, 400)           #elevation min and max
+npeaks    = 4                   #number of peaks
+landscape = 150                 #number of patches on each side, total patch number = landscape*landscape
+
+nindvs    = 500                 #number of individuals to simulate
+nsteps    = 1000                #number of steps an individual can take
+move      = 0.8                 #decimal likelihood of individual moving to highest neighbor patch (R&G call this q)
+
+#initialize landscape
+
+#initialize individuals on landscape
+
+#allow individuals to move within landscape
+
+#extract needed output from simulation
+
